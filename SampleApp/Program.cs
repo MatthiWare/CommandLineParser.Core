eff6f381344d7162f56ef3d79ca5685660edeca4
Matthias Beerens <beerens.matthias@hotmail.com>
2018-09-30 15:05:42 +0200

    Update sample app

--- SampleApp/Program.cs
+++ SampleApp/Program.cs
@@ -1,15 +1,63 @@
 ﻿using System;
+using MatthiWare.CommandLine;
 
 namespace SampleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            foreach (var arg in args)
-                Console.WriteLine(arg);
+            var parser = new CommandLineParser<Options>();
+
+            // setup
+            parser.Configure(opt => opt.MyInt)
+                .ShortName("-i")
+                .LongName("--int")
+                .Required();
+
+            parser.Configure(opt => opt.MyString)
+                .ShortName("-s")
+                .LongName("--string")
+                .Required();
+
+            parser.Configure(opt => opt.MyBool)
+                .ShortName("-b")
+                .LongName("--bool")
+                .Required();
+
+            parser.Configure(opt => opt.MyDouble)
+                .ShortName("-d")
+                .LongName("--double")
+                .Required();
+
+            var result = parser.Parse(args);
+
+            if (result.HasErrors)
+            {
+                Console.Error.WriteLine(result.Error);
+                Console.ReadKey();
+
+                return -1;
+            }
+
+            var options = result.Result;
+
+            Console.WriteLine($"MyInt: {options.MyInt}");
+            Console.WriteLine($"MyString: {options.MyString}");
+            Console.WriteLine($"MyBool: {options.MyBool}");
+            Console.WriteLine($"MyDouble: {options.MyDouble}");
 
             Console.ReadKey();
+
+            return 0;
+        }
+
+        private class Options
+        {
+            public int MyInt { get; set; }
+            public string MyString { get; set; }
+            public bool MyBool { get; set; }
+            public double MyDouble { get; set; }
         }
     }
 }
