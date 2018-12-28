36a029135cd5fdbe8ddf698fdf43c35965a39384
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2018-12-28 11:55:43 +0100

    Add comments

--- CommandLineParser/Abstractions/Command/ICommandExecutor.cs
+++ CommandLineParser/Abstractions/Command/ICommandExecutor.cs
@@ -2,6 +2,11 @@
 
 namespace MatthiWare.CommandLine.Abstractions.Command
 {
+    /// <summary>
+    /// API for configurion command executions
+    /// </summary>
+    /// <typeparam name="TOption">Base option</typeparam>
+    /// <typeparam name="TSource">Command option</typeparam>
     public interface ICommandExecutor<TOption, TSource>
         where TOption : class
         where TSource : class, new()
@@ -14,10 +19,25 @@ namespace MatthiWare.CommandLine.Abstractions.Command
         /// <returns><see cref="ICommandBuilder{TOption,TSource}"/></returns>
         ICommandBuilder<TOption, TSource> InvokeCommand(bool invoke);
 
+        /// <summary>
+        /// Sets the command execute action
+        /// </summary>
+        /// <param name="action">Action to execute</param>
+        /// <returns><see cref="ICommandBuilder{TOption,TSource}"/></returns>
         ICommandBuilder<TOption, TSource> OnExecuting(Action action);
 
+        /// <summary>
+        /// Sets the command execute action
+        /// </summary>
+        /// <param name="action">Action to execute</param>
+        /// <returns><see cref="ICommandBuilder{TOption,TSource}"/></returns>
         ICommandBuilder<TOption, TSource> OnExecuting(Action<TOption> action);
 
+        /// <summary>
+        /// Sets the command execute action
+        /// </summary>
+        /// <param name="action">Action to execute</param>
+        /// <returns><see cref="ICommandBuilder{TOption,TSource}"/></returns>
         ICommandBuilder<TOption, TSource> OnExecuting(Action<TOption, TSource> action);
     }
 }
