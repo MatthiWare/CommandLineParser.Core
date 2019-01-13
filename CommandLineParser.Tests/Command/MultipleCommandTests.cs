dbed5b203737057a640b6ad9400157983f060fb4
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2019-01-13 13:37:17 +0100

    Add test for multiple non required commands with required options

---
+++ CommandLineParser.Tests/Command/MultipleCommandTests.cs
@@ -0,0 +1,38 @@
+﻿using MatthiWare.CommandLine;
+using MatthiWare.CommandLine.Core.Attributes;
+using Xunit;
+
+namespace MatthiWare.CommandLineParser.Tests.Command
+{
+    public class MultipleCommandTests
+    {
+        [Theory]
+        [InlineData(new string[] { "cmd1", "-x", "8" }, false)]
+        [InlineData(new string[] { "cmd2", "-x", "8" }, false)]
+        [InlineData(new string[] { }, false)]
+        public void NonRequiredCommandShouldNotSetResultInErrorStateWhenRequiredOptionsAreMissing(string[] args, bool hasErrors)
+        {
+            var parser = new CommandLineParser<object>();
+
+            parser.AddCommand<MultipleCOmmandTestsOptions>()
+                .Name("cmd1")
+                .Required(false)
+                .Description("cmd1");
+
+            parser.AddCommand<MultipleCOmmandTestsOptions>()
+                .Name("cmd2")
+                .Required(false)
+                .Description("cmd2");
+
+            var result = parser.Parse(args);
+
+            Assert.False(result.HasErrors);
+        }
+
+        private class MultipleCOmmandTestsOptions
+        {
+            [Required, Name("x", "bla"), Description("some description")]
+            public int Option { get; set; }
+        }
+    }
+}
