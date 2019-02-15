94b32f261ece78789bee36a4efaedb4b7bfacfc0
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2019-02-15 19:27:46 +0100

    Add failing test case

--- CommandLineParser.Tests/CustomerReportedTests.cs
+++ CommandLineParser.Tests/CustomerReportedTests.cs
@@ -1,11 +1,14 @@
-﻿using MatthiWare.CommandLine;
-
+﻿using System;
+using System.Collections.Generic;
+using MatthiWare.CommandLine;
+using MatthiWare.CommandLine.Core.Attributes;
 using Xunit;
 
 namespace MatthiWare.CommandLine.Tests
 {
     public class CustomerReportedTests
     {
+        #region Issue_12
         /// <summary>
         /// Running with *no* parameters at all crashes the command line parser #12
         /// https://github.com/MatthiWare/CommandLineParser.Core/issues/12
@@ -35,5 +38,55 @@ namespace MatthiWare.CommandLine.Tests
         {
             public int Test { get; set; }
         }
+        #endregion
+
+        #region Issue_30_AutoPrintUsageAndErrors
+        /// <summary>
+        /// AutoPrintUsageAndErrors always prints even when everything is *fine* #30
+        /// https://github.com/MatthiWare/CommandLineParser.Core/issues/30
+        /// </summary>
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData("true", null)]
+        [InlineData("false", null)]
+        [InlineData(null, "bla")]
+        public void AutoPrintUsageAndErrorsShouldNotPrintWhenEverythingIsFIne(string verbose, string path)
+        {
+            var parser = new CommandLineParser<OptionsModelIssue_30>();
+
+            var items = new List<string>();
+            AddItemToArray(verbose);
+            AddItemToArray(path);
+
+            var parsed = parser.Parse(items.ToArray());
+
+            if (parsed.HasErrors)
+            {
+                foreach (var err in parsed.Errors)
+                    throw err;
+            }
+
+            void AddItemToArray(string item)
+            {
+                if (item != null)
+                    items.Add(item);
+            }
+        }
+
+        private class OptionsModelIssue_30
+        {
+            [Required]
+            [Name("v", "verb")]
+            [DefaultValue(true)]
+            [Description("Verbose description")]
+            public bool Verbose { get; set; }
+
+            [Required]
+            [Name("p", "path")]
+            [DefaultValue(@"C:\Some\Path")]
+            [Description("Path description")]
+            public bool Path { get; set; }
+        }
+        #endregion
     }
 }
