db6bf2bf2699e130cf7ac56dd88d58be7ba0710b
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2019-08-16 14:01:57 +0200

    Add non happy path

---
+++ CommandLineParser.Tests/Command/RegisterCommandTests.cs
@@ -0,0 +1,32 @@
+﻿using MatthiWare.CommandLine.Abstractions.Command;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MatthiWare.CommandLine.Tests.Command
+{
+    public class RegisterCommandTests
+    {
+        [Fact]
+        public void RegisterCommandWithNoneCommandTypeThrowsException()
+        {
+            var parser = new CommandLineParser();
+
+            Assert.Throws<ArgumentException>(() => parser.RegisterCommand(typeof(object)));
+        }
+
+        [Fact]
+        public void RegisterCommandWithWrongParentTypeThrowsException()
+        {
+            var parser = new CommandLineParser();
+
+            Assert.Throws<ArgumentException>(() => parser.RegisterCommand(typeof(MyWrongCommand)));
+        }
+
+        private class MyWrongCommand : Command<Exception, Exception>
+        {
+
+        }
+    }
+}
