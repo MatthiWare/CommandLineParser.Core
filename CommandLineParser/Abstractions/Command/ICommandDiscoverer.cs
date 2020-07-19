317140eb4d8b62f08e73697ae0ec86c0bf06e280
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2020-07-19 15:45:19 +0200

    Add command disoverer

---
+++ CommandLineParser/Abstractions/Command/ICommandDiscoverer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace MatthiWare.CommandLine.Abstractions.Command
+{
+    public interface ICommandDiscoverer
+    {
+        IReadOnlyList<Type> DiscoverCommandTypes(Assembly assembly);
+        IReadOnlyList<Type> DiscoverCommandTypes(Assembly[] assemblies);
+    }
+}
---
+++ CommandLineParser/Core/Command/CommandDiscoverer.cs
@@ -0,0 +1,40 @@
+﻿using MatthiWare.CommandLine.Abstractions.Command;
+using MatthiWare.CommandLine.Core.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace MatthiWare.CommandLine.Core.Command
+{
+    public class CommandDiscoverer : ICommandDiscoverer
+    {
+        public IReadOnlyList<Type> DiscoverCommandTypes(Assembly assembly)
+            => DiscoverCommandTypes(new[] { assembly });
+
+        public IReadOnlyList<Type> DiscoverCommandTypes(Assembly[] assemblies)
+        {
+            List<Type> foundCommands = new List<Type>();
+
+            foreach (var assembly in assemblies)
+            {
+                FindCommandsInAssembly(assembly, foundCommands);
+            }
+
+            return foundCommands.AsReadOnly();
+        }
+
+        private void FindCommandsInAssembly(Assembly assembly, List<Type> list)
+            => list.AddRange(assembly.ExportedTypes.Where(IsValidCommandType));
+
+        private static bool IsValidCommandType(Type type)
+        {
+            if (type.IsAbstract || !type.IsClass)
+            {
+                return false;
+            }
+
+            return type.IsAssignableToGenericType(typeof(Command<>));
+        }
+    }
+}
