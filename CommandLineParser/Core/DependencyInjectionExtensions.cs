489685a49555d131cd28e613cf7b8d96601cf28f
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2020-09-06 13:53:01 +0200

    Improve extension method to allow chaining

--- CommandLineParser/Core/DependencyInjectionExtensions.cs
+++ CommandLineParser/Core/DependencyInjectionExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using MatthiWare.CommandLine.Abstractions;
 using MatthiWare.CommandLine.Abstractions.Command;
-using MatthiWare.CommandLine.Abstractions.Models;
 using MatthiWare.CommandLine.Abstractions.Parsing;
 using MatthiWare.CommandLine.Abstractions.Usage;
 using MatthiWare.CommandLine.Abstractions.Validations;
@@ -15,26 +14,58 @@ using Microsoft.Extensions.Logging.Abstractions;
 
 namespace MatthiWare.CommandLine.Core
 {
+    /// <summary>
+    /// Extension methods to allow DI services to be registered.
+    /// </summary>
     public static class DependencyInjectionExtensions
     {
-        public static void AddInternalCommandLineParserServices<TOption>(this IServiceCollection services, CommandLineParser<TOption> parser, CommandLineParserOptions options)
+        /// <summary>
+        /// Adds internal services used by <see cref="CommandLineParser"/>. 
+        /// This won't overwrite existing services.
+        /// </summary>
+        /// <typeparam name="TOption">Base option type</typeparam>
+        /// <param name="services">Current service collection</param>
+        /// <param name="parser">Current instance reference</param>
+        /// <param name="options">Current options reference</param>
+        /// <returns>Input service collection to allow method chaining</returns>
+        public static IServiceCollection AddInternalCommandLineParserServices<TOption>(this IServiceCollection services, CommandLineParser<TOption> parser, CommandLineParserOptions options)
             where TOption : class, new()
+        {
+            return services
+                .AddCommandLineParser(parser)
+                .AddValidatorContainer()
+                .AddParserOptions(options)
+                .AddDefaultResolvers()
+                .AddCLIPrinters()
+                .AddCommandDiscoverer()
+                .AddEnvironmentVariables()
+                .AddDefaultLogger();
+        }
+
+        private static IServiceCollection AddParserOptions(this IServiceCollection services, CommandLineParserOptions options)
+        {
+            services.AddSingleton(options);
+
+            return services;
+        }
+
+        private static IServiceCollection AddCommandLineParser<TOption>(this IServiceCollection services, CommandLineParser<TOption> parser)
+             where TOption : class, new()
         {
             services.AddSingleton<ICommandLineCommandContainer>(parser);
             services.AddSingleton<ICommandLineParser<TOption>>(parser);
 
-            services.AddSingleton<IValidatorsContainer, ValidatorsContainer>();
-
-            services.AddSingleton(options);
-
-            services.AddDefaultResolvers();
-            services.AddCLIPrinters();
-            services.AddCommandDiscoverer();
-            services.AddEnvironmentVariables();
-            services.AddLogging();
+            return services;
         }
 
-        public static void AddDefaultResolvers(this IServiceCollection services)
+        private static IServiceCollection AddValidatorContainer(this IServiceCollection services)
+        {
+            services.AddSingleton<IValidatorsContainer, ValidatorsContainer>();
+
+            return services;
+        }
+
+        private static IServiceCollection AddDefaultResolvers(this IServiceCollection services)
         {
             services.TryAddScoped<IArgumentResolver<bool>, BoolResolver>();
             services.TryAddScoped<IArgumentResolver<double>, DoubleResolver>();
@@ -42,27 +73,37 @@ namespace MatthiWare.CommandLine.Core
             services.TryAddScoped<IArgumentResolver<string>, StringResolver>();
 
             services.TryAddScoped(typeof(IArgumentResolver<>), typeof(DefaultResolver<>));
+
+            return services;
         }
 
-        public static void AddCLIPrinters(this IServiceCollection services)
+        private static IServiceCollection AddCLIPrinters(this IServiceCollection services)
         {
             services.TryAddScoped<IUsageBuilder, UsageBuilder>();
             services.TryAddScoped<IUsagePrinter, UsagePrinter>();
+
+            return services;
         }
 
-        public static void AddCommandDiscoverer(this IServiceCollection services)
+        private static IServiceCollection AddCommandDiscoverer(this IServiceCollection services)
         {
             services.TryAddScoped<ICommandDiscoverer, CommandDiscoverer>();
+
+            return services;
         }
 
-        public static void AddEnvironmentVariables(this IServiceCollection services)
+        private static IServiceCollection AddEnvironmentVariables(this IServiceCollection services)
         {
             services.TryAddScoped<IEnvironmentVariablesService, EnvironmentVariableService>();
+
+            return services;
         }
 
-        public static void AddLogging(this IServiceCollection services)
+        private static IServiceCollection AddDefaultLogger(this IServiceCollection services)
         {
             services.TryAddSingleton<ILoggerFactory, NullLoggerFactory>();
+
+            return services;
         }
     }
 }
