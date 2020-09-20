e165800bae8dc473a9f24b60715225c7e6936ddc
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2020-09-20 17:28:53 +0200

    Fix build, upgrade fluent validation https://docs.fluentvalidation.net/en/latest/upgrading-to-9.html

--- Extensions/FluentValidationsExtensions/Core/FluentTypeValidatorCollection`1.cs
+++ Extensions/FluentValidationsExtensions/Core/FluentTypeValidatorCollection`1.cs
@@ -1,4 +1,5 @@
-﻿using MatthiWare.CommandLine.Abstractions;
+﻿using FluentValidation;
+using MatthiWare.CommandLine.Abstractions;
 using MatthiWare.CommandLine.Abstractions.Validations;
 using MatthiWare.CommandLine.Core;
 using System;
@@ -30,7 +31,7 @@ namespace MatthiWare.CommandLine.Extensions.FluentValidations.Core
         public IValidationResult Validate(object @object)
         {
             var errors = validators.Get()
-                .Select(v => v.Validate(@object))
+                .Select(v => v.Validate(new ValidationContext<object>(@object)))
                 .SelectMany(r => r.Errors)
                 .ToList();
 
@@ -47,7 +48,7 @@ namespace MatthiWare.CommandLine.Extensions.FluentValidations.Core
         public async Task<IValidationResult> ValidateAsync(object @object, CancellationToken cancellationToken = default)
         {
             var errors = (await Task.WhenAll(validators.Get()
-                .Select(async v => await v.ValidateAsync(@object, cancellationToken))))
+                .Select(async v => await v.ValidateAsync(new ValidationContext<object>(@object), cancellationToken))))
                 .SelectMany(r => r.Errors)
                 .ToList();
 
