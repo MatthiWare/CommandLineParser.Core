0f878dc09039ca8f0e1d67e6ff39923a9c1c4a79
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2020-09-28 15:45:12 +0200

    Use new ArgumentManager everywhere

--- CommandLineParser/CommandLineParser`TOption.cs
+++ CommandLineParser/CommandLineParser`TOption.cs
@@ -186,7 +186,6 @@ namespace MatthiWare.CommandLine
 
             var result = new ParseResult<TOption>();
 
-            //var argumentManager = new ArgumentManager(args, ParserOptions, m_helpOptionName, m_helpOptionNameLong, m_commands, m_options.Values.Cast<ICommandLineOption>().ToArray());
             var argumentManager = new ArgumentManager2(ParserOptions, this);
 
             argumentManager.Process(args);
@@ -220,13 +219,15 @@ namespace MatthiWare.CommandLine
 
             var result = new ParseResult<TOption>();
 
-            var argumentManager = new ArgumentManager(args, ParserOptions, m_helpOptionName, m_helpOptionNameLong, m_commands, m_options.Values.Cast<ICommandLineOption>().ToArray());
+            var argumentManager = new ArgumentManager2(ParserOptions, this);
+
+            argumentManager.Process(args);
 
             await ParseCommandsAsync(errors, result, argumentManager, cancellationToken);
 
             ParseOptions(errors, result, argumentManager);
 
-            //CheckForExtraHelpArguments(result, argumentManager);
+            CheckForExtraHelpArguments(result, argumentManager);
 
             await ValidateAsync(m_option, errors, cancellationToken);
 
