d019ba6a29d8e23f9d53435d2ce43fc6be79d3a7
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2020-10-05 10:20:53 +0200

    Add global parser tests for all datatypes

--- CommandLineParser.Tests/Parsing/Resolvers/GlobalParserTests.cs
+++ CommandLineParser.Tests/Parsing/Resolvers/GlobalParserTests.cs
@@ -13,6 +13,150 @@ namespace MatthiWare.CommandLine.Tests.Parsing.Resolvers
         {
         }
 
+        [Fact]
+        public void ParseByte()
+        {
+            var parser = new CommandLineParser<CollectionModel>(Services);
+
+            var result = parser.Parse(new[] { "-byte", "1" });
+
+            result.AssertNoErrors();
+
+            Assert.Equal(1, result.Result.Byte);
+        }
+
+        [Fact]
+        public void ParseString()
+        {
+            var parser = new CommandLineParser<CollectionModel>(Services);
+
+            var result = parser.Parse(new[] { "-str", "text" });
+
+            result.AssertNoErrors();
+
+            Assert.Equal("text", result.Result.String);
+        }
+
+        [Fact]
+        public void ParseSByte()
+        {
+            var parser = new CommandLineParser<CollectionModel>(Services);
+
+            var result = parser.Parse(new[] { "-sbyte", "1" });
+
+            result.AssertNoErrors();
+
+            Assert.Equal(1, result.Result.Sbyte);
+        }
+
+        [Fact]
+        public void ParseInt()
+        {
+            var parser = new CommandLineParser<CollectionModel>(Services);
+
+            var result = parser.Parse(new[] { "-int", "1" });
+
+            result.AssertNoErrors();
+
+            Assert.Equal(1, result.Result.Int);
+        }
+
+        [Fact]
+        public void ParseLong()
+        {
+            var parser = new CommandLineParser<CollectionModel>(Services);
+
+            var result = parser.Parse(new[] { "-long", "1" });
+
+            result.AssertNoErrors();
+
+            Assert.Equal(1, result.Result.Long);
+        }
+
+        [Fact]
+        public void ParseULong()
+        {
+            var parser = new CommandLineParser<CollectionModel>(Services);
+
+            var result = parser.Parse(new[] { "-ulong", "1" });
+
+            result.AssertNoErrors();
+
+            Assert.Equal<ulong>(1, result.Result.Ulong);
+        }
+
+        [Fact]
+        public void ParseUShort()
+        {
+            var parser = new CommandLineParser<CollectionModel>(Services);
+
+            var result = parser.Parse(new[] { "-ushort", "1" });
+
+            result.AssertNoErrors();
+
+            Assert.Equal<ushort>(1, result.Result.Ushort);
+        }
+
+        [Fact]
+        public void ParseShort()
+        {
+            var parser = new CommandLineParser<CollectionModel>(Services);
+
+            var result = parser.Parse(new[] { "-short", "1" });
+
+            result.AssertNoErrors();
+
+            Assert.Equal(1, result.Result.Short);
+        }
+
+        [Fact]
+        public void ParseUint()
+        {
+            var parser = new CommandLineParser<CollectionModel>(Services);
+
+            var result = parser.Parse(new[] { "-uint", "1" });
+
+            result.AssertNoErrors();
+
+            Assert.Equal<uint>(1, result.Result.Uint);
+        }
+
+        [Fact]
+        public void ParseDecimal()
+        {
+            var parser = new CommandLineParser<CollectionModel>(Services);
+
+            var result = parser.Parse(new[] { "-decimal", "1.0m" });
+
+            result.AssertNoErrors();
+
+            Assert.Equal(1.0m, result.Result.Decimal);
+        }
+
+        [Fact]
+        public void ParseFloat()
+        {
+            var parser = new CommandLineParser<CollectionModel>(Services);
+
+            var result = parser.Parse(new[] { "-long", "1.0f" });
+
+            result.AssertNoErrors();
+
+            Assert.Equal(1.0f, result.Result.Float);
+        }
+
+        [Fact]
+        public void ParseBool()
+        {
+            var parser = new CommandLineParser<CollectionModel>(Services);
+
+            var result = parser.Parse(new[] { "-bool", "true" });
+
+            result.AssertNoErrors();
+
+            Assert.True(result.Result.Bool);
+        }
+
         [Theory]
         [InlineData(new string[] { "-int-arr", "1", "2", "3" }, true)]
         [InlineData(new string[] { "-int-arr", "1", "-int-arr", "2", "-int-arr", "3" }, true)]
@@ -87,15 +231,40 @@ namespace MatthiWare.CommandLine.Tests.Parsing.Resolvers
 
         private class CollectionModel
         {
+            [Name("bool")]
+            public bool Bool { get; set; }
+            [Name("int")]
+            public int Int { get; set; }
+            [Name("str")]
+            public string String { get; set; }
+            [Name("short")]
+            public short Short { get; set; }
+            [Name("long")]
+            public long Long { get; set; }
+            [Name("double")]
+            public double Double { get; set; }
+            [Name("ushort")]
+            public ushort Ushort { get; set; }
+            [Name("ulong")]
+            public ulong Ulong { get; set; }
+            [Name("byte")]
+            public byte Byte { get; set; }
+            [Name("sbyte")]
+            public sbyte Sbyte { get; set; }
+            [Name("uint")]
+            public uint Uint { get; set; }
+            [Name("decimal")]
+            public decimal Decimal { get; set; }
+            [Name("float")]
+            public float Float { get; set; }
             [Name("int-arr")]
             public int[] IntArray { get; set; }
-
             [Name("str-arr")]
             public string[] StrArray { get; set; }
-
             [Name("int-list")]
             public List<int> IntList { get; set; }
-
+            [Name("int-iset")]
+            public ISet<int> IntISet { get; set; }
             [Name("int-set")]
             public HashSet<int> IntSet { get; set; }
         }
