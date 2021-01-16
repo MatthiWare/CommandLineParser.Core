16a2be1b0246596a1214d68d2f3418c00c59ba5c
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2021-01-16 17:05:11 +0100

    Add new constructor for #120

--- CommandLineParser/CommandLineParser.cs
+++ CommandLineParser/CommandLineParser.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.ComponentModel;
 
 namespace MatthiWare.CommandLine
 {
@@ -26,6 +28,8 @@ namespace MatthiWare.CommandLine
         /// Creates a new instance of the commandline parser
         /// </summary>
         /// <param name="serviceCollection">Services collection to use, if null will create an internal one</param>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        [Obsolete("Use extension method 'AddCommandLineParser' to register the parser with DI")]
         public CommandLineParser(IServiceCollection serviceCollection)
             : this(new CommandLineParserOptions(), serviceCollection)
         { }
@@ -35,8 +39,27 @@ namespace MatthiWare.CommandLine
         /// </summary>
         /// <param name="parserOptions">options that the parser will use</param>
         /// <param name="serviceCollection">Services collection to use, if null will create an internal one</param>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        [Obsolete("Use extension method 'AddCommandLineParser' to register the parser with DI")]
         public CommandLineParser(CommandLineParserOptions parserOptions, IServiceCollection serviceCollection)
             : base(parserOptions, serviceCollection)
+        { }
+
+        /// <summary>
+        /// Creates a new instance of the commandline parser
+        /// </summary>
+        /// <param name="serviceProvider">Services provider to use</param>
+        public CommandLineParser(IServiceProvider serviceProvider)
+            : this(new CommandLineParserOptions(), serviceProvider)
+        { }
+
+        /// <summary>
+        /// Creates a new instance of the commandline parser
+        /// </summary>
+        /// <param name="parserOptions">options that the parser will use</param>
+        /// <param name="serviceProvider">Services Provider to use</param>
+        public CommandLineParser(CommandLineParserOptions parserOptions, IServiceProvider serviceProvider)
+            : base(parserOptions, serviceProvider)
         { }
     }
 }
